14001cf8ad21684d3f230a416b6b64f9b15b6a1d
DenisKantic <denis.kantic18@gmail.com>
2020-12-10 09:22:38 +0100

    added another function in JS for not letting user to submit form with login button if we didn't input username or password

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ function checkInput(event){
 
     event.preventDefault();
 
+    if(usernameBox.value == ""){
+        inputMessage.innerHTML = "Please enter your username";
+    } else if (passwordBox.value == ""){
+        inputMessage.style.color = "black";
+        inputMessage.innerHTML = "Please enter your password";
+    } else {
+
     if(usernameBox.value == "user" && passwordBox.value == "1234"){
             inputMessage.style.color = "green";
             inputMessage.innerHTML = "Login successful";
@@ -17,4 +24,5 @@ function checkInput(event){
         inputMessage.style.color = "red";
         inputMessage.innerHTML = "Username or password is incorrect";
     }
+}
 }
